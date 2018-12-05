b833330a12ae1700bc182038bfa32be9ad3bda60
Ernesto Rodriguez Ortiz <erozqba@gmail.com>
2018-12-05 09:43:45 -0500

    Issue 229: Add explicit encoding, license and unix line separators.
    Some maintainers for different linux distribution are having some
    problems with the encoding on different python versions. This change
    intents to make all python files define explicitly the encoding, license
    and unix line separators. Remove one example of the README.rst file
    where a possible non UTF-8 character is used.

--- num2words/lang_UK.py
+++ num2words/lang_UK.py
@@ -14,6 +14,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301 USA
+
 from __future__ import unicode_literals
 
 from .base import Num2Word_Base
